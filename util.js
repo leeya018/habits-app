4774de44e31c67c9717d3a9bdbdac53b1812500d
lee yahav <leeyahav018@gmail.com>
2023-04-30 12:00:30 +0300

    game is fix with validi

--- util.js
+++ util.js
@@ -1,9 +1,16 @@
+const isValid = (thrill) => {
+  return eval(thrill) !== NaN && eval(thrill) >= 0 && eval(thrill) !== Infinity;
+};
+
 export const createThrill = () => {
-  const actions = ["+", "-", "*", "/"];
-  const randAction = actions[Math.floor(Math.random() * 4)];
-  const randNumber1 = Math.floor(Math.random() * 10);
-  const randNumber2 = Math.floor(Math.random() * 10);
-  const thrill = `${randNumber1} ${randAction} ${randNumber2} `;
+  let thrill;
+  const actions = ["+", "-", "*"];
+  do {
+    const randAction = actions[Math.floor(Math.random() * actions.length)];
+    const randNumber1 = Math.floor(Math.random() * 10);
+    const randNumber2 = Math.floor(Math.random() * 10);
+    thrill = `${randNumber1} ${randAction} ${randNumber2} `;
+  } while (!isValid(thrill));
   return thrill;
 };
 
