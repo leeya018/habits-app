4e4525241146f250ae21222e248f2cbc8baaa6b3
lee yahav <leeyahav018@gmail.com>
2023-05-01 13:56:55 +0300

    sealevel steps

--- components/SeaLevel.jsx
+++ components/SeaLevel.jsx
@@ -5,9 +5,15 @@ export default function SeaLevel({}) {
   const { level } = useSelector((state) => state.math);
 
   return (
-    <div
-      className="absolute w-[100%] bg-blue-400 bottom-0"
-      style={{ top: level + "px" }}
-    ></div>
+    <>
+      <div
+        className="absolute w-[100%] bg-blue-400 bottom-0"
+        style={{ top: level + "px" }}
+      ></div>
+      <div className="absolute border-4 w-6 h-2 border-yellow-400 top-[550px]"></div>
+      <div className="absolute border-4 w-6 h-2 border-yellow-400 top-[600px]"></div>
+      <div className="absolute border-4 w-6 h-2 border-yellow-400 top-[650px]"></div>
+      <div className="absolute border-4 w-6 h-2 border-yellow-400 top-[700px]"></div>
+    </>
   );
 }
