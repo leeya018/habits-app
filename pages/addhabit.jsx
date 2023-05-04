21d97ea02d83ed6882bf2da1a2571adb15350a61
lee yahav <leeyahav018@gmail.com>
2023-05-04 19:17:09 +0300

    fix

--- pages/addhabit.jsx
+++ pages/addhabit.jsx
@@ -1,4 +1,4 @@
-import { addHabit } from "actions";
+import { addHabit, updateError } from "actions";
 import Button from "components/habits/Button";
 import Input from "components/habits/Input";
 import { useRouter } from "next/router";
@@ -15,53 +15,64 @@ export default function AddHabit({}) {
     mainGoal: "",
   });
   const dispatch = useDispatch();
-  const { habits } = useSelector((state) => state.habits);
+  const { habits, error } = useSelector((state) => state.habits);
 
   const updateHabit = ({ name, value }) => {
+    dispatch(updateError());
+
     setHabit((prev) => ({ ...prev, [name]: value }));
   };
 
+  const checkValidation = () => {
+    let keysLeft = Object.keys(habit).filter((key) => habit[key] === "");
+    console.log(keysLeft);
+    const isAllSet = keysLeft.length === 0;
+
+    return isAllSet;
+  };
   const addNewHabit = () => {
-    const newHabit = {
-      ...habit,
-      id: uuidv4(),
-      createdAt: new Date().toDateString(),
-      amountCompletePerDay: {},
-    };
-    dispatch(addHabit(newHabit));
+    if (checkValidation()) {
+      const newHabit = {
+        ...habit,
+        id: uuidv4(),
+        createdAt: new Date().toDateString(),
+        amountCompletePerDay: {},
+      };
+      dispatch(addHabit(newHabit));
+      setHabit({ name: "", description: "", amount: "", mainGoal: "" });
+    } else {
+      dispatch(updateError("one of the fields is not set"));
+    }
   };
   return (
     <div className="flex justify-center">
       <div className="flex flex-col">
         <h1>NEW HABIT</h1>
         <Input
-          type="text"
           name="name"
           value={habit.name}
           onChange={(e) => updateHabit(e.target)}
         />
         <Input
-          type="text"
           name="description"
           value={habit.description}
           onChange={(e) => updateHabit(e.target)}
         />
         <Input
-          type="text"
           name="amount"
           value={habit.amount}
           onChange={(e) => updateHabit(e.target)}
         />
         <Input
-          type="text"
-          value={habit.mainGaol}
+          value={habit.mainGoal}
           name="mainGoal"
           onChange={(e) => updateHabit(e.target)}
         />
         <Button onClick={addNewHabit}>add new habit</Button>
-        <div>{JSON.stringify(habits)}</div>
+        {/* <div>{JSON.stringify(habits)}</div> */}
         <div>{JSON.stringify(habit)}</div>
         <Button onClick={() => router.push("/allhabits")}>go to habits</Button>
+        <div className="bg-yellow-400 text-red-600 font-bold">{error}</div>
       </div>
     </div>
   );
