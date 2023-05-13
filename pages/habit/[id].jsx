a914a7f3425122a6f221cd88af25b09875793f67
lee yahav <leeyahav018@gmail.com>
2023-05-13 23:38:00 +0300

    delete fix

--- pages/habit/[id].jsx
+++ pages/habit/[id].jsx
@@ -3,6 +3,9 @@ import Habit from "components/habits/Habit";
 import * as API from "lib/api";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import * as Action from "actions";
+import { useDispatch, useSelector } from "react-redux";
+import Modal from "components/habits/Modal";
 
 export const getServerSideProps = async (context) => {
   const id = context.query.id;
@@ -18,11 +21,29 @@ export const getServerSideProps = async (context) => {
 
 export default function HabitItem({ habit }) {
   const router = useRouter();
+  const dispatch = useDispatch();
+  const { showModal, chosenGoal, chosenHabit } = useSelector(
+    (state) => state.habits
+  );
 
   useEffect(() => {}, [habit]);
 
+  const removeHabit = () => {
+    dispatch(Action.deleteHabit(chosenHabit.id, chosenHabit.goal));
+    dispatch(Action.updateModalShow(false));
+    dispatch(Action.updateChosenHabit(null));
+    router.push(`/goal/${chosenGoal.id}/addhabit`);
+  };
+
   return (
     <div>
+      <Modal
+        isShow={showModal}
+        title="Habit Deletion"
+        text="Are you sue you want to delete habit"
+        textButton="Delete Habit"
+        onClick={removeHabit}
+      />
       <div className="flex absolute top-1 left-1 gap-1">
         <Button color="bg-gray_dark" onClick={() => router.back()}>
           back{" "}
